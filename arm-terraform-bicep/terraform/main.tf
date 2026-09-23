de779dfc48996f3e49082b3351d6b0eeff10d0fc
Mahesh Patel <heymaheshpatelloveit@gmail.com>
2026-09-23 15:17:07 -0400

    Update Terraform storage replication to GRS

--- arm-terraform-bicep/terraform/main.tf
+++ arm-terraform-bicep/terraform/main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "az104" {
   location = "East US"
 }
 resource "azurerm_storage_account" "az104" {
-  name                = "patelterraformstorage001"
-  resource_group_name = azurerm_resource_group.az104.name
-  location            = "eastus"
-  account_kind        = "StorageV2"
+  name                     = "patelterraformstorage001"
+  resource_group_name      = azurerm_resource_group.az104.name
+  location                 = "eastus"
+  account_kind             = "StorageV2"
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 }
